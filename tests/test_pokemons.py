93796a84556100f73a53ced90ad0250eed5561fd
Aristarh Gran <aristarhgrann@yandex.ru>
2024-05-27 12:27:42 +0300

    add tests

--- tests/test_pokemons.py
+++ tests/test_pokemons.py
@@ -2,22 +2,17 @@ import requests
 import pytest
 
 URL = 'https://api.pokemonbattle.me/v2'
-TOKEN = 'a64b9fa06bf1159f0a649558ea12f3a3'
+TOKEN = '993735695d6996dc045269871e0fe693'
 HEADER = {
     "Content-Type": "application/json",
     "trainer_token": TOKEN
 }
-TRAINER_ID = '4281'
+TRAINER_ID = '2402'
 
 def test_status_code():
-    response_get_pokemons = requests.get(url=f'{URL}/pokemons', params={'trainer_id': TRAINER_ID})
-    assert response_get_pokemons.status_code == 200
+    response_get_trainers = requests.get(url=f'{URL}/trainers')
+    assert response_get_trainers.status_code == 200
 
-def test_part_of_response():
-    response_get = requests.get(url=f'{URL}/pokemons', params={'trainer_id': TRAINER_ID})
-    assert response_get.json()["data"][0]["name"] == "Бульбазавр"
-
-@pytest.mark.parametrize('key, value', [('name', 'Бульбазавр'), ('trainer_id', TRAINER_ID), ('id', '28442')])
-def test_parametrize(key, value):
-    response_parametrize = requests.get(url=f'{URL}/pokemons', params={'trainer_id': TRAINER_ID})
-    assert response_parametrize.json()["data"][0][key] == value
+def test_get_my_trainer():
+    response_get_my_trainer = requests.get(url=f'{URL}/trainers', params={'trainer_id': TRAINER_ID})
+    assert response_get_my_trainer.json()["data"][0]["id"] == "2402"
